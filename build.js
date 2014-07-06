8e034585b2a18523f601595831af83d9b0ca5b19
Alex <j201.alex@gmail.com>
2014-07-06 16:04:53 -0400

    Continue on build system

--- build.js
+++ build.js
@@ -1,14 +1,19 @@
 var fs = require('fs');
-var mustache = require('mustache');
+var mustache = require('mustache').render;
 var metaMarked = require('meta-marked');
 var highlight = require('highlight.js');
 var wrench = require('wrench');
+var moment = require('moment');
 
 function readFile(file) {
 	return fs.readFileSync(file, { encoding: "utf-8" });
 }
 
-marked.setOptions({
+function copyFileSync(from, to) {
+	fs.writeFileSync(to, fs.readFileSync(from, { encoding: 'utf8' }));
+}
+
+metaMarked.setOptions({
 	highlight: function (code, lang) {
 		return lang ? 
 			highlight.highlight(lang, code).value :
@@ -16,14 +21,32 @@ marked.setOptions({
 	}
 });
 
+function parseDate(str) {
+	return moment(str, "MMM DD, YYYY");
+}
+
 wrench.rmdirSyncRecursive('public', function() {}); // fail silently - it might not be there
 fs.mkdirSync('public');
+fs.mkdirSync('public/posts');
 
 var posts = fs.readdirSync('posts').map(function(post) {
 	return metaMarked(readFile('posts/' + post));
 }).sort(function(a, b) {
-	return Date.parse(a.meta.date) - Date.parse(b.meta.date);
+	return parseDate(a.meta.date).unix() - parseDate(b.meta.date).unix();
+}).map(function(post) {
+	post.date = parseDate(post.date);
+	post.url = "/posts/" + post.date.format("YYYY-MM-DD") + "-" + encodeURIComponent(post.meta.title.replace(/\s+/g, '-')) + ".html";
+	return post;
 });
 
-posts.forEach(function(post) {
+var templates = {
+	master: readFile("templates/master.html"),
+	post: readFile("templates/post.html"),
+	// postLink: readFile("templates/post-link.html"),
+	// projects: readFile("templates/projects.html")
+};
 
+posts.forEach(function(post) {
+	var postHTML = mustache(templates.master, {postList: '', content: mustache(templates.post, {title: post.title, content: post.html})});
+	fs.writeFileSync("public" + post.url, postHTML);
+});
